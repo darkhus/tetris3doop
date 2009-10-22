3307a92d6c05ed020ab8891ed2f387cdd1082917
darkhus <darkhus@ab75283e-bb0f-11de-8784-f7ad4773dbab>
2009-10-22 09:48:01 +0000

    komenty dla java doc +

--- Tetris3d/src/main/AppletJOGL.java
+++ Tetris3d/src/main/AppletJOGL.java
@@ -4,11 +4,22 @@ import java.awt.*;
 import javax.media.opengl.*;
 import javax.swing.JApplet;
 
-
+/**
+ * This class creates applets for JOGL display
+ * @author Daras
+ */
 public class AppletJOGL extends JApplet{
-	
+
+    /**
+     * game canvas
+     */
 	TetrisGL canvas;
 
+    /**
+     * create canvas accordin to hardware capabilities,
+     * alse enabls 2x antialiasing
+     * @return game canvas
+     */
 	public TetrisGL makeCanvas()
 	{
         GLCapabilities capabilities = new GLCapabilities();
@@ -19,6 +30,9 @@ public class AppletJOGL extends JApplet{
 	    return new TetrisGL(capabilities);
 	}
 
+    /**
+     * initialize applet, starts game
+     */
 	public void init() {
         new Thread().start();
 		setLayout(new BorderLayout());
@@ -28,13 +42,21 @@ public class AppletJOGL extends JApplet{
 		add(canvas, BorderLayout.CENTER);
 	}
 
-	public void start() {
-		
+    /**
+     * applet starts
+     */
+	public void start() {		
 	}
 
+    /**
+     * applet stops
+     */
 	public void stop() {
 	}
-    
+
+    /**
+     * applet closed, destroy all
+     */
     public void destroy(){
         canvas.destroyAll();
     }
